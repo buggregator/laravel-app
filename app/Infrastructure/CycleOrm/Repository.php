b282fa8adae98f03a05e7541386ad9f546f2b295
butschster <butschster@gmail.com>
2021-11-22 21:46:16 +0300

    Removes redundant repository methods

--- app/Infrastructure/CycleOrm/Repository.php
+++ app/Infrastructure/CycleOrm/Repository.php
@@ -4,21 +4,11 @@ declare(strict_types=1);
 
 namespace Infrastructure\CycleOrm;
 
-use Cycle\ORM\Select;
 use Cycle\ORM\Select\Repository as BaseRepository;
-use Cycle\ORM\Transaction;
-use Cycle\ORM\TransactionInterface;
 use Illuminate\Support\Collection;
 
 class Repository extends BaseRepository
 {
-    public function __construct(
-        Select $select,
-        private TransactionInterface $transaction
-    ) {
-        parent::__construct($select);
-    }
-
     public function findAll(array $scope = [], array $orderBy = []): Collection
     {
         return $this->newCollection(
@@ -29,29 +19,5 @@ class Repository extends BaseRepository
     private function newCollection(array $items): Collection
     {
         return new Collection($items);
-    }
-
-    public function store(object $entity, bool $cascade = true, bool $run = true): void
-    {
-        $this->transaction->persist(
-            $entity,
-            $cascade ? Transaction::MODE_CASCADE : Transaction::MODE_ENTITY_ONLY
-        );
-
-        if ($run) {
-            $this->transaction->run(); // transaction is clean after run
-        }
-    }
-
-    public function delete(object $entity, bool $cascade = true, bool $run = true)
-    {
-        $this->transaction->delete(
-            $entity,
-            $cascade ? Transaction::MODE_CASCADE : Transaction::MODE_ENTITY_ONLY
-        );
-
-        if ($run) {
-            $this->transaction->run(); // transaction is clean after run
-        }
     }
 }
