3f62f63e199b2ddfdfc6718be4ea866440faee93
butschster <butschster@gmail.com>
2021-12-03 18:13:57 +0300

    Adds app:configure console command Updates Readme

--- app/Interfaces/Console/Kernel.php
+++ app/Interfaces/Console/Kernel.php
@@ -6,10 +6,13 @@ namespace Interfaces\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Interfaces\Console\Commands\ConfigureApp;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [];
+    protected $commands = [
+        ConfigureApp::class
+    ];
 
     protected function schedule(Schedule $schedule)
     {
