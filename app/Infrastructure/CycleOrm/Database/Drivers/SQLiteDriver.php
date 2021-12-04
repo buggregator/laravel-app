a35231eb1cd9f96cb5a6417fbe752703ea4b774a
butschster <butschster@gmail.com>
2021-12-04 13:44:59 +0300

    Fixes custom Sqlite driver creation

--- app/Infrastructure/CycleOrm/Database/Drivers/SQLiteDriver.php
+++ app/Infrastructure/CycleOrm/Database/Drivers/SQLiteDriver.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace Infrastructure\CycleOrm\Database\Drivers;
 
+use Cycle\Database\Config\DriverConfig;
+use Cycle\Database\Driver\SQLite\SQLiteCompiler;
+use Cycle\Database\Driver\SQLite\SQLiteHandler;
+use Cycle\Database\Query\QueryBuilder;
 use Illuminate\Contracts\Cache\Lock;
 use Illuminate\Support\Facades\Cache;
 
 class SQLiteDriver extends \Cycle\Database\Driver\SQLite\SQLiteDriver
 {
+    public static function create(DriverConfig $config): self
+    {
+        return new self(
+            $config,
+            new SQLiteHandler(),
+            new SQLiteCompiler('""'),
+            QueryBuilder::defaultBuilder()
+        );
+    }
+
     private function getLock(): Lock
     {
         return Cache::lock('driver.'.$this->getType(), 10);
@@ -24,6 +38,6 @@ class SQLiteDriver extends \Cycle\Database\Driver\SQLite\SQLiteDriver
     public function disconnect(): void
     {
         parent::disconnect();
-        $this->getLock()->forceRelease();
+        $this->getLock()->release();
     }
 }
